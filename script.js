d35a4a0f28e2dc109fdd8ebb33172dc1d3572fc5
shail1999-blip <chudawkarshailesh@gmail.com>
2025-12-27 20:11:37 +0530

    Add files via upload
    new home page

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-const openPopupBtn = document.querySelector('.open-popup-btn');
-const backdrop = document.querySelector('.backdrop');
-const closeBtn = document.querySelector('.close-btn');
+function searchTutorial() {
+  const value = document.getElementById("search").value;
 
-// Open popup
-openPopupBtn.addEventListener('click', () => {
-    backdrop.style.display = 'flex';
-});
-
-// Close popup
-closeBtn.addEventListener('click', () => {
-    backdrop.style.display = 'none';
-});
-
-// Close popup when clicking outside of it
-backdrop.addEventListener('click', (e) => {
-    if (e.target === backdrop) {
-        backdrop.style.display = 'none';
-    }
-});
-
-const showModalBtn = document.querySelector('.show-modal-btn');
-const overlay = document.querySelector('.overlay');
-const dismissBtn = document.querySelector('.dismiss-btn');
-
-// Open popup
-showModalBtn.addEventListener('click', () => {
-    overlay.style.display = 'flex';
-});
-
-// Close popup
-dismissBtn.addEventListener('click', () => {
-    overlay.style.display = 'none';
-});
-
-// Close popup when clicking outside of it
-overlay.addEventListener('click', (e) => {
-    if (e.target === overlay) {
-        overlay.style.display = 'none';
-    }
-});
-
+  if (value.trim() === "") {
+    alert("Please enter a tutorial name!");
+  } else {
+    alert("Searching for: " + value);
+  }
+}
